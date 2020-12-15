2ea1dae7385a6d3797f63cd92105966d863d9142
indahtambunan <55008554+indahtambunan@users.noreply.github.com>
2020-12-15 10:54:41 +0700

    ubah

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,6 +2,6 @@
     <div class="float-right d-none d-sm-block">
         <b>Version</b> 3.0.4
     </div>
-    <strong>Copyright &copy; 2014-2019 <a href="#">Lelang Desain</a>.</strong> All rights
+    <strong>Copyright &copy; 2014-2019 <a href="#">ArsiTech</a>.</strong> All rights
     reserved.
 </footer>
